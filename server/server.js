15befe7655e058bcb6885e7e5aa51623cae41c8e
Kamla23 <kamlabai23@navgurukul.org>
2026-06-07 18:46:06 +0530

    update

--- server/server.js
+++ server/server.js
@@ -8,8 +8,18 @@ import errorMiddleware from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 const app = express();
-app.use(cors());
 
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://lead-management-crm-1.onrender.com"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -27,4 +37,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT} `); 
 });
-
